23d5d8b9bafb75723a6dad2acbbdcbbf73eec7ba
Timur Yakshibaev <timayakshi@gmail.com>
2025-10-31 19:54:02 +0500

    изменил выбор персонажа

--- work/practice5/prac6.py
+++ work/practice5/prac6.py
@@ -2,29 +2,41 @@ class Hero:
     def __init__(self, name, hp):
         self.name = name
         self.hp = hp
-    def equipment(self, var):
-        equip = {
-            '1' : {'armor' : 'Легкая броня', 'weapon' : 'Копье'},
-            '2' : {'armor' : 'Средняя броня', 'weapon' : 'Булава'},
-            '3' : {'armor' : 'Тяжелая броня', 'weapon' : 'Меч'},
-            '4' : {'armor' : 'Мантия', 'weapon' : 'Лук'}
+
+    def armor(self, armor):
+        armor_dict = {
+            'light': 'Кожанка',
+            'middle': 'Кольчуга',
+            'heavy': 'Латный доспех',
+            'ultra_heavy': 'Рыцарские доспехи',
         }
-        for pick in equip:
-            if pick == var:
-                return equip[pick]
-    def get_data(self):
-        self.equip = new_player.equipment(pick)
-        print('Добро пожаловать!',self.name,',','Ваше снаряжение:', self.equip)
-    def attack(self, damage):
-        if damage > self.hp:
-            self.hp = 0
-        else:
-            self.hp -= damage
+        return armor_dict.get(armor)
+
+    def weapon(self, weapon):
+        weapon_dict = {
+            'melee_low_range': 'Кинжалы',
+            'melee_middle_range': 'Рапира',
+            'melee_long_range': 'Двуручный меч',
+            'ranger_light': 'Арбалет',
+            'ranger_middle': 'Лук',
+            'ranger_heavy': 'Мушкет',
+        }
+        return weapon_dict.get(weapon)
+
+    def get_data(self, pick_armor, pick_weapon):
+        self.pick_armor = self.armor(pick_armor)
+        self.pick_weapon = self.weapon(pick_weapon)
+        print('Добро пожаловать!', self.name, 'Ваша броня:', self.pick_armor, 'Ваше оружие:', self.pick_weapon)
+
+
+name = input('Введите имя вашего персонажа: ')
+new_player = Hero(name, 100)
+
+pick_armor = input('Выбор брони: (light), (middle), (heavy), (ultra_heavy): ').lower()
+pick_weapon = input('Выбор оружия: (melee_low_range), (melee_middle_range), (melee_long_range), (ranger_light), (ranger_middle), (ranger_heavy): ')
+
+new_player.get_data(pick_armor, pick_weapon)
+
 
                 
 
-
-new_player = Hero(input('Введите имя вашего персонажа: '), 100)
-pick = input('Выбор снаряжения - (Крестьянин - 1, Пехотинец - 2, Рыцарь - 3, Лучник - 4): ')
-equip_pick = new_player.equipment(pick)
-data = new_player.get_data()
